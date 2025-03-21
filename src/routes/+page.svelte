8d26079aa91ccd66e191499b56b7566f0d738119
Sahanaj1 <sahanajoshi07@gmail.com>
2025-03-21 20:36:10 +0530

    update: state and reactive forms

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,65 @@
-<script>
-let count = $state(0);
+<!-- <script>
+  import Header from "./Header.svelte";
+  let count = $state(0);
 
-function increment (){
-
-    count+=1;
-}
+  function increment() {
+    count += 1;
+    console.log(name,'name')
+  }
+  let name= $state();
 </script>
 
-<h1 >
-hii {count}
-</h1>
+<Header {name} />
+<h1>hii {count}</h1>
+<button onclick={increment}> Click me </button>
+<input bind:value={name} type="text"/> -->
 
-<button onclick={increment}>
-Click me
-</button>
+<script>
+    import Header from "./Header.svelte";
+
+    let formState = $state({
+        name: '',
+        birthday: '',
+        step:0,
+        error:'',
+    });
+</script>
+
+
+
+{#if formState.error!==""}
+<p style="color:red;">{formState.error}</p>
+{/if}
+<main>
+    {#if formState.step==0}
+    <h1>{formState.step}</h1>
+    <form>
+        <label for="name">Name:</label>
+        <input type="text" id="name" bind:value={formState.name}/>
+        <br>
+        <button onclick={()=>{
+            if (formState.name !==''){
+                formState.step +=1;
+                formState.error = "";
+            }else{
+                formState.error = "Please enter your name"
+            }
+        }}>next</button>
+    </form>
+    {:else if formState.step==1}
+    <h1>{formState.step}</h1>
+    <form>
+        <label for="birthday">Birthday:</label>
+        <input type="date" id="birthday" bind:value={formState.birthday}/>
+        <br>
+        <button onclick={()=>{
+            if (formState.birthday !==''){
+                formState.step +=1;
+                formState.error = "";
+            }else{
+                formState.error = "Please select your bday"
+            }
+        }}>next</button>
+    </form>
+    {/if}
+</main>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,6 @@
+<script>
+    let {name} = $props();
+</script>
+
+<h1>Header</h1>
+<h2>props: {name}</h2>
